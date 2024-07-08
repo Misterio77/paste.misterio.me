e15ae725c8fa9ad3f83e00fa42a73d91bc39bbe9
Gabriel Fontes <hi@m7.rs>
2024-07-08 20:26:10 -0300

    chore: bump flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Paste service and companion CLI tool";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     systems.url = "github:nix-systems/default";
   };
 
